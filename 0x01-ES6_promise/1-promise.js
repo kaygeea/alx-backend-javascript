23fb62e108b025bf0827cfdc11975200b7295885
Koyejo <adinlewakoyejo@yahoo.com>
2023-10-27 15:27:28 +0000

    Add func that returns a resolved & rejected promise

---
+++ 0x01-ES6_promise/1-promise.js
@@ -0,0 +1,14 @@
+export default function getFullResponseFromAPI(success) {
+  return new Promise((resolve, reject) => {
+    if(success) {
+      const successTruthy = {
+        'status': 200,
+	'body': 'Success'
+      };
+      resolve(successTruthy);
+    } else {
+      const successFalsy = 'The fake API is not working currently'
+      reject(new Error(successFalsy));
+    }
+  })
+}
