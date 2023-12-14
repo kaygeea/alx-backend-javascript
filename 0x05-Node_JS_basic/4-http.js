dabdebd8748e38bc0de198cb32f16eddd0e4b277
Koyejo <adinlewakoyejo@yahoo.com>
2023-12-14 17:12:03 +0000

    Add server export

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,10 +5,12 @@ const port = 1245;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain')
+  res.setHeader('Content-Type', 'text/plain');
   res.end('Hello Holberton School!');
 });
 
 app.listen(port, hostname, () => {
   process.stdout.write(`Server is running and listening on port ${port}`);
 });
+
+module.exports = app;
