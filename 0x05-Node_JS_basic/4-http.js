1bcfdd216b35ea4cd65089e8a83d0528b507ccf0
Koyejo <adinlewakoyejo@yahoo.com>
2023-12-14 17:06:21 +0000

    Add additional server responses

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -5,9 +5,10 @@ const port = 1245;
 
 const app = http.createServer((req, res) => {
   res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain')
   res.end('Hello Holberton School!');
 });
 
 app.listen(port, hostname, () => {
-  console.log(`Server is running and listening on port ${port}`);
+  process.stdout.write(`Server is running and listening on port ${port}`);
 });
