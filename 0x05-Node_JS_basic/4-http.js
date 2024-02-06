dcda7b66a5a5e3ac22a4596c6cabbde64e038991
Koyejo <adinlewakoyejo@yahoo.com>
2024-02-06 11:01:35 +0000

    Add module to create small HTTP server

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
+
+app.on('request', (req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+  res.response(200);
+  res.end('Hello Holberton School!');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
+});
+
+module.exports = app;
