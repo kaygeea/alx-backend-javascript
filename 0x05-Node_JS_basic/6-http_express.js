e60f50ad945fa32b9e5379e261a9b906dd14855b
Koyejo <adinlewakoyejo@yahoo.com>
2024-02-08 17:19:16 +0000

    Add function to create exppress server

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const PORT = 1245;
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running and listening on Port: ${PORT}`);
+})
