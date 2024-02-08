8cfb797b4f3df400e6244fcc152cce2c8a617194
Koyejo <adinlewakoyejo@yahoo.com>
2024-02-08 11:35:10 +0000

    Add logic to handle missing file case

--- 0x05-Node_JS_basic/5-http.js
+++ 0x05-Node_JS_basic/5-http.js
@@ -7,51 +7,56 @@ const DB_FILE = process.argv[2] || '';
 
 // STEP 1 - Define function to count students, for the '/students' route handler
 const countStudent = (dbPath) => new Promise((resolve, reject) => {
-  fs.readFile(dbPath, 'utf-8', (err, data) => {
-    if (err) {
-      reject(new Error('Cannot load the Database'));
-      return;
-    }
-    if (data) {
-      // Parse CSV content into an array of arrays
-      const arrOfFileContent = data.trim().split('\n').map((line) => line.split(','));
-
-      // Convert array of arrays to object, with the csv headers as the keys
-      const header = arrOfFileContent[0];
-
-      // Define and map values to their respective keys
-      const studentDataObj = arrOfFileContent.slice(1).map((value) => {
-        const person = {};
-        header.forEach((key, index) => {
-          person[key] = value[index];
-        });
-        return person;
-      });
-
-      // Process object to get student data
-      let csStudents = '';
-      let csStudentCount = 0;
-      let sweStudents = '';
-      let sweStudentCount = 0;
-      let fieldOne = '';
-      let fieldTwo = '';
-
-      for (const student of studentDataObj) {
-        if (student.field === 'CS') {
-          csStudents += `${student.firstname}, `;
-          csStudentCount += 1;
-          fieldOne = student.field;
-        } else if (student.field === 'SWE') {
-          sweStudents += `${student.firstname}, `;
-          sweStudentCount += 1;
-          fieldTwo = student.field;
-        }
+  if (!dbPath) {
+    reject(new Error('Cannot load the database'));
+  }
+  if (dbPath) {
+    fs.readFile(dbPath, 'utf-8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the Database'));
+        return;
       }
-      const message = `This is the list of our students\nNumber of students: ${studentDataObj.length}\nNumber of students in ${fieldOne}: ${csStudentCount}. List: ${csStudents.slice(0, -2)}\nNumber of students in ${fieldTwo}: ${sweStudentCount}. List: ${sweStudents.slice(0, -2)}`;
+      if (data) {
+        // Parse CSV content into an array of arrays
+        const arrOfFileContent = data.trim().split('\n').map((line) => line.split(','));
 
-      resolve(message); // Resolve the promise with the message to be returned on the page
-    }
-  });
+        // Convert array of arrays to object, with the csv headers as the keys
+        const header = arrOfFileContent[0];
+
+        // Define and map values to their respective keys
+        const studentDataObj = arrOfFileContent.slice(1).map((value) => {
+          const person = {};
+          header.forEach((key, index) => {
+            person[key] = value[index];
+          });
+          return person;
+        });
+
+        // Process object to get student data
+        let csStudents = '';
+        let csStudentCount = 0;
+        let sweStudents = '';
+        let sweStudentCount = 0;
+        let fieldOne = '';
+        let fieldTwo = '';
+
+        for (const student of studentDataObj) {
+          if (student.field === 'CS') {
+            csStudents += `${student.firstname}, `;
+            csStudentCount += 1;
+            fieldOne = student.field;
+          } else if (student.field === 'SWE') {
+            sweStudents += `${student.firstname}, `;
+            sweStudentCount += 1;
+            fieldTwo = student.field;
+          }
+        }
+        const message = `This is the list of our students\nNumber of students: ${studentDataObj.length}\nNumber of students in ${fieldOne}: ${csStudentCount}. List: ${csStudents.slice(0, -2)}\nNumber of students in ${fieldTwo}: ${sweStudentCount}. List: ${sweStudents.slice(0, -2)}`;
+
+        resolve(message); // Resolve the promise with the message to be returned on the page
+      }
+    });
+  };
 });
 
 // STEP 2 - Define the route handlers that the received request would be sent to
