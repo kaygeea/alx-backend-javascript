c40de97dade251e27f8e20f95a831cdcef349073
Koyejo <adinlewakoyejo@yahoo.com>
2024-02-09 12:38:54 +0000

    Add ctrl'er with static methdos for students route

---
+++ 0x05-Node_JS_basic/full_server/controllers/StudentController.js
@@ -0,0 +1,57 @@
+import readDatabase from '../utils'
+
+const DB_FILE = process.argv[2] || '';
+
+/**
+ * Controller for handling student-related routes.
+ * @module full_server/controllers/StudentController
+ * @class
+ */
+export class StudentsController {
+  /**
+  * Retrieves all students from the database and sends the response.
+  * @static
+  * @param {Object} request - The HTTP request object.
+  * @param {Object} response - The HTTP response object.
+  * @see module:full_server\utils.js
+  * @returns {void}
+  */
+  static getAllStudents(request, response) {
+    let msg = '';
+
+    readDatabase(DB_FILE).then((objOfArr) => {
+      for (const key in objOfArr) {
+        msg += `Number of students in ${key}: ${objOfArr[key].length}. List: ${objOfArr[key].join(', ')}\n`;
+      }
+      response.send(msg.trim());
+    }).catch((error) => {
+      response.status(500).send(error.message);
+    });
+  }
+
+  /**
+  * Retrieves students, by major, from the database and sends the response.
+  * @static
+  * @param {Object} request - The HTTP request object.
+  * @param {Object} response - The HTTP response object.
+  * @throws {Error} Will throw an error if the wrong parameter is received.
+  * @see module:full_server\utils.js
+  * @returns {void}
+  */
+  static getAllStudentsByMajor(request, response) {
+    let msg = '';
+    const major = (request.param.major || '').toUpperCase();
+
+    if (major === 'CS' || major === 'SWE') {
+      readDatabase(DB_FILE).then((objOfArr) => {
+        msg = `Number of students in ${major}: ${objOfArr[major].length}. List: ${objOfArr[major].join(', ')}`
+        response.send(msg);
+      }).catch((error) => {
+        response.status(500).send(error.message);
+      })
+    } else {
+      response.status(500).send('Major parameter must be CS or SWE');
+    }
+  }
+}
+
