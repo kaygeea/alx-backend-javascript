cf592d4ff7a7e54a5b135a58a2e1c23ef54a7c54
Koyejo <adinlewakoyejo@yahoo.com>
2024-02-09 13:18:16 +0000

    Fix eslint errors

--- 0x05-Node_JS_basic/full_server/routes/index.js
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { AppController } from '../controllers/AppController';
 import { StudentsController } from '../controllers/StudentsController';
-export const router = express.Router();
+
+const router = express.Router();
 
 // Define routes
 router.get('/', AppController.getHomePage);
 router.get('/students', StudentsController.getAllStudents);
 router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+
+export default router;
