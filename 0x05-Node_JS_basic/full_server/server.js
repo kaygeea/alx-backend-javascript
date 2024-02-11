5e86b7d219f725924467a471dda4e302440cad10
Koyejo <adinlewakoyejo@yahoo.com>
2024-02-11 08:02:56 +0000

    Correct export statement

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,13 +1,15 @@
 import express from 'express';
-import { router } from './routes/index';
+import router from './routes/index';
 
 const app = express();
-const PORT = 1245 || process.env.PORT;
+const PORT = 1245;
 
-// Use routes middleware
+// Use router middleware
 app.use('/', router);
 
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server is running and listening on PORT ${PORT}`);
 });
+
+export default app;
